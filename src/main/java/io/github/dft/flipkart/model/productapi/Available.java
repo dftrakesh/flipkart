e736026473f0d4d47566b2777ec84c98ad787eab
jinal.panchal <jinal.panchal@dakshyafreondtech.com>
2023-08-07 17:54:04 +0530

    In Flipkart-SDK:  - change return type of getProductById method.  - change SKU pojo.  - update version of SDK.  - use Hash map in Available.java

--- src/main/java/io/github/dft/flipkart/model/productapi/Available.java
+++ src/main/java/io/github/dft/flipkart/model/productapi/Available.java
@@ -1,16 +1,19 @@
 package io.github.dft.flipkart.model.productapi;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.List;
+
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Available {
 
-    private Sku sku;
-    private Integer ordersToBeFulfilled;
+    private HashMap<String, Sku> available;
+    private List<String> invalid;
 }
